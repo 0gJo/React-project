aef95fd0347faf38e0fc0dbcef13bb59c7d21644
0gJo <youngjijo@YOUNGJIs-MacBook-Pro.local>
2021-01-13 09:21:41 +0900

    header 컴포넌트 추가

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import React from "react"; // 안쓰더라도, 반드시 명시해줘야함 (react 라는 scope를 명시하는 것 )
 import { gql } from 'apollo-boost';
-import Router from "./Router";
-import {ThemeProvider} from 'styled-components';
+import { HashRouter as Router } from 'react-router-dom';
+import Routes from "./Routes";
 
 import {useQuery} from 'react-apollo-hooks'
 
+import styled, {ThemeProvider} from 'styled-components';
 import Theme from '../Styles/Theme';
+import GlobalStyles from '../Styles/GlobalStyles';
 
 import Header from './Header';
 
@@ -15,14 +17,28 @@ const QUERY = gql`
 }
 `;
 
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: ${props => props.theme.maxWidth};
+  width: 100%;
+`;
+
 
 export default () => {
     const {  data: { isLoggedIn }} = useQuery(QUERY);
 
     return (
         <ThemeProvider theme={Theme}>
-        {isLoggedIn && <Header />}   
-        <Router isLoggedIn={isLoggedIn} />
+            <>
+            <GlobalStyles />
+        <Router>
+         <Header/>
+            <Wrapper>
+                <Routes isLoggedIn={isLoggedIn}/>
+            </Wrapper>
+
+        </Router>
+        </>
         </ThemeProvider>
     )
 }
--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PropTypes from "prop-types";
 import Auth from "../Routes/Auth";
 import Home from "./Home";
@@ -18,10 +18,7 @@ const LoggedOutRoutes = () => (
 
 
 const AppRouter = ({ isLoggedIn }) => (
-    <Router>
         <Switch>{isLoggedIn ? <LoggedInRoutes/>: <LoggedOutRoutes/>}</Switch>
-    </Router>
-
 )
 
 //PropTypes를 이용해서 React 컴포넌트에 prop으로 넘어오는 입력값들이 어떤 타입을 가져야하는지 정의
