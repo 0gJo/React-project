47f8e7e270496a32e4cccae045f5a1bb9295a488
0gJo <youngjijo@YOUNGJIs-MacBook-Pro.local>
2021-01-29 00:25:45 +0900

    #7.3 Post Component part Three 댓글창 & useEffect를 이용한 자동 slider

--- src/Components/Post/PostContainer.js
+++ src/Components/Post/PostContainer.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import PostPresenter from './PostPresenter';
 import useInput from '../../Hooks/useInput'
@@ -7,6 +7,7 @@ const PostContainer = (
         id,
         user,
         files,
+        location,
         likeCount,
         isLiked,
         comments,
@@ -15,17 +16,36 @@ const PostContainer = (
 ) => {
     const [isLikedS, setIsLiked] = useState(isLiked);
     const [likeCountS, setLikeCount] = useState(likeCount);
+    const [currentItem, setCurrentItem] = useState(0);
+    //슬라이드 넘기는 함수 
+    const slide = () => {
+        const totalFiles = files.length;
+        if (currentItem === totalFiles - 1) {
+          setTimeout(() => setCurrentItem(0), 3000);
+        } else {
+          setTimeout(() => setCurrentItem(currentItem + 1), 3000);
+        }
+      };
+
+      useEffect(() => {
+        slide();
+      }, [currentItem]); //currentItem 값이 변경되면 다시 slide 실행 
+
+      //결과적으로 계속 슬라이딩됨
+
     const comment = useInput("");
     return <PostPresenter
         user={user}
         files={files}
         likeCount={likeCountS}
+        location={location}
         isLiked={isLikedS}
         comments={comments}
         createdAt={createdAt}
         newComment={comment}
         setIsLiked={setIsLiked}
         setLikeCount={setLikeCount}
+        currentItem={currentItem}
     />
 }
 
