9c09c59c3b62be68296bfdc4596289607f6e8d1f
0gJo <youngjijo@YOUNGJIs-MacBook-Pro.local>
2021-02-05 23:31:50 +0900

    search 기능 완료

---
+++ src/Routes/Search/SearchQueries.js
@@ -0,0 +1,22 @@
+import { gql } from "apollo-boost";
+
+
+// 2개의 쿼리를 한번에 질의 가능 
+export const SEARCH = gql`
+  query search($term: String!) {
+    searchPost(term: $term) {
+      files {
+        url
+      }
+      likeCount
+      commentCount
+    }
+    searchUser(term: $term) {
+      id
+      avatar
+      username
+      isFollowing
+      isSelf
+    }
+  }
+`;
